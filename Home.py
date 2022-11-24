e118999a529afabf1ecc9f1211c03d66ae26f813
agata <agatazebryk7@gmail.com>
2022-11-24 13:32:06 +0100

    Encoding issue v1.

--- Home.py
+++ Home.py
@@ -3,9 +3,13 @@ import streamlit as st
 import tabula
 from tabula import read_pdf
 
+@st.cache
+def convert_df_to_csv(df):
+    # IMPORTANT: Cache the conversion to prevent computation on every rerun
+    return df.to_csv().encode('latin2')
 
 
-st.header('Menu ZK')
+st.header('Menu ZK sprawdzam polskie znaki śćśłż')
 
 
 # Upload File
@@ -14,18 +18,14 @@ if uploaded_file is not None:
     df = read_pdf(uploaded_file, encoding='latin2', pages='all', pandas_options={'header': None})
     st.write(pd.DataFrame(df[0]))
     df_1 = pd.DataFrame(df[0])
+    output_df = convert_df_to_csv(df_1)
 
 
-# Downlaod File
-@st.cache
-def convert_df_to_csv(df):
-    # IMPORTANT: Cache the conversion to prevent computation on every rerun
-    return df.to_csv().encode('latin2')
+    # Downlaod File
 
-output_df = convert_df_to_csv(df_1)
-st.download_button(
+    st.download_button(
     label="Download data as CSV",
     data=output_df,
     file_name='out.csv',
     mime='text/csv',
-)
+    )
