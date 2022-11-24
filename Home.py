9f8e1a9ad2d93dcd12aaaac24df7b9b225a76e39
agata <agatazebryk7@gmail.com>
2022-11-24 12:51:22 +0100

    Initial commit

---
+++ Home.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import streamlit as st
+import tabula
+from tabula import read_pdf
+
+
+
+st.header('Menu ZK')
+
+
+# Upload File
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+    df = read_pdf(uploaded_file, encoding='latin2', pages='all', pandas_options={'header': None})
+    st.write(pd.DataFrame(df[0]))
+    df_1 = pd.DataFrame(df[0])
+
+
+# Downlaod File
+@st.cache
+def convert_df_to_csv(df):
+    # IMPORTANT: Cache the conversion to prevent computation on every rerun
+    return df.to_csv().encode('latin2')
+
+output_df = convert_df_to_csv(df_1)
+st.download_button(
+    label="Download data as CSV",
+    data=output_df,
+    file_name='out.csv',
+    mime='text/csv',
+)
