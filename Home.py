ea5e4c23283df93715ae793619185f03cec64038
agata <agatazebryk7@gmail.com>
2022-12-07 19:34:53 +0100

    Zareba added.

--- Home.py
+++ Home.py
@@ -27,6 +27,16 @@ def one_columns_convert(file_name):
         df_full = df_full.append(df_temp)
     return df_full
 
+def zareba_convert(file_name):
+    df = read_pdf(file_name, encoding='latin2', pages='1', pandas_options={'header': None},
+                  area=[90, 31, 828, 576], guess=False)
+    df_full = pd.DataFrame(df[0])
+    df = read_pdf(file_name, encoding='latin2', pages='all', pandas_options={'header': None})
+    for i in range(1, len(df)):
+        df_temp = pd.DataFrame(df[i])
+        df_full = df_full.append(df_temp)
+    return df_full
+
 def bemowo_convert(file_name):
     # Lewa kolumna
     df = read_pdf(file_name, encoding='latin2', pages='all', pandas_options={'header': None},
@@ -99,6 +109,8 @@ def jaslo_convert(file_name):
     return df_final
 
 
+
+
 jedna = ['Jelenia Góra', 'Poznań', 'Inowrocław', 'Włocławek']
 
 ### Do Dodanie  1:   'Zaręba'   3:     'Sieradz', 'Jasło', 'Rzeszów', 'Wejherowo'
@@ -112,6 +124,10 @@ uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
     if zaklad in jedna:
         df = one_columns_convert(uploaded_file)
+        st.write(df)
+        output_df = convert_df_to_csv(df)
+    elif zaklad == 'Zaręba':
+        df = zareba_convert(uploaded_file)
         st.write(df)
         output_df = convert_df_to_csv(df)
     elif zaklad == 'Bemowo':
